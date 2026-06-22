e608836d3c087ade3f78fc23ff88015b4aeecd47
Joao-Felipe-coding <jofelipe2018@gmail.com>
2026-06-22 19:34:00 -0300

    feat: adiciona entidade User, DTOs e configuração do banco MySQL

---
+++ user_service/src/user/user.dto.ts
@@ -0,0 +1,12 @@
+export class CreateUserDTO {
+  nome!: string;
+  cpf!: string;
+  telefone!: string;
+  matricula!: string;
+  senha!: string;
+}
+
+export class LoginDTO {
+  matricula!: string;
+  senha!: string;
+}
---
+++ user_service/src/user/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  AutoIncrement,
+  Column,
+  DataType,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
+
+@Table
+export class User extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column({ type: DataType.INTEGER })
+  declare id: number;
+
+  @Column({ type: DataType.STRING })
+  nome!: string;
+
+  @Column({ type: DataType.STRING })
+  cpf!: string;
+
+  @Column({ type: DataType.STRING })
+  telefone!: string;
+
+  @Column({ type: DataType.STRING })
+  matricula!: string;
+
+  @Column({ type: DataType.STRING })
+  senha!: string;
+}
