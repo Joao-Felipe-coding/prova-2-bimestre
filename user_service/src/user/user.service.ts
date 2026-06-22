f63764e95fbf7ef96bc9bda321ff3e3a63642080
Joao-Felipe-coding <jofelipe2018@gmail.com>
2026-06-22 19:38:00 -0300

    feat: implementa método create_user no service com hash bcrypt

---
+++ user_service/src/user/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import * as bcrypt from 'bcrypt';
+import { User } from './user.entity';
+import { CreateUserDTO } from './user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(User) private user: typeof User) {}
+
+  async create_user(data: CreateUserDTO) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(data.senha, salt);
+
+    return this.user.create({
+      nome: data.nome,
+      cpf: data.cpf,
+      telefone: data.telefone,
+      matricula: data.matricula,
+      senha: hash,
+    });
+  }
+}
