0c172f3c6d09ed8604108e8778ad7a0c4c1049f5
Joao-Felipe-coding <jofelipe2018@gmail.com>
2026-06-22 19:54:00 -0300

    feat: organiza módulo principal com Sequelize e UserModule

--- user_service/src/app.module.ts
+++ user_service/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { DatabaseConfig } from './database/database.config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfig,
+    }),
+    UserModule,
   ],
   controllers: [],
   providers: [],
